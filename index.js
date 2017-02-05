45c15f42544e3950a2949445490cb2cd9bf780b9
Talon Bragg <pvpgamecrash@gmail.com>
2017-02-05 09:43:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(process.env.PORT || 3000);
